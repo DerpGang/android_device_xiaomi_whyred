40da0d52a42a081ab17796393a9854ebc525effd
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    whyred: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -26,6 +26,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+    BuildDesc="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys" \
+    BuildFingerprint=xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys \
+    DeviceName=whyred \
+    DeviceProduct=whyred
 
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
